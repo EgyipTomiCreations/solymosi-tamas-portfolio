4cf4781a559c5d02bdf3e35d8a951c00c65d4ad9
Solymosi Tamás <96898224+EgyipTomiCreations@users.noreply.github.com>
2025-02-23 16:45:42 +0100

    First version

--- README.md
+++ README.md
@@ -1,36 +1,22 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+# 🌟 My Portfolio - Built with Next.js 🚀
 
-## Getting Started
+Welcome to my personal portfolio site! This project was bootstrapped with [**Next.js**](https://nextjs.org) and serves as a showcase of my work, skills, and projects. 💻✨
 
-First, run the development server:
+## 🎯 Getting Started
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+To get started with the development server, run:
+
+```
+npm run dev  # or yarn dev, pnpm dev, bun dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Then, open [**localhost:3000**](http://localhost:3000) in your browser to view the site. 🎨
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+## 🛠 Editing & Customization
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+-   Modify `app/page.tsx` to update the homepage. 🏠
+    
+-   Enjoy **hot reloading** as you edit—no need to restart! 🔥
+    
 
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+Thanks for visiting! 🎉
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,199 @@
+import React from "react";
+import { FlipWords } from "../components/ui/flip-words";
+import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
+import { GlowingEffect } from "@/components/ui/glowing-effect";
 import Image from "next/image";
+import { FiExternalLink } from "react-icons/fi";
+import { FloatingDock } from "@/components/ui/floating-dock"; import {
+  IconBrandGithub,
+  IconHome,
+  IconBrandLinkedin,
+  IconMail
+} from "@tabler/icons-react";
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const words = ["backend", ".NET", "C#", "Blazor", "mobile", "web", "student"];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <div>
+      {/* Hero and Languages Section filling one viewport */}
+      <div className="min-h-screen flex flex-col">
+        {/* Hero Section */}
+        <div className="flex-grow flex justify-center items-center px-4">
+          <div className="text-4xl mx-auto font-normal text-neutral-95 text-neutral-300">
+            Hi! I&apos;m Tamas a <FlipWords words={words} /> <br />
+            developer from Hungary
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {/* Languages Section */}
+        <div className="flex flex-col items-center justify-center pb-12">
+          <h2 className="text-1xl font-semibold text-neutral-100 text-neutral-200">
+            Languages and frameworks I mainly use
+          </h2>
+          <div className="mt-4 rounded-md flex flex-col antialiased dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+            <InfiniteMovingCards items={techstack} direction="right" speed="slow" />
+          </div>
+        </div>
+      </div>
+
+      {/* Projects Section - Requires scrolling to reach */}
+      <div className="pt-12">
+        <div className="flex flex-col items-center justify-center">
+          <h2 className="text-1xl font-semibold text-neutral-100 text-neutral-200">
+            Projects I&apos;m proud of
+          </h2>
+        </div>
+        <div className="px-8 flex flex-col items-center justify-center py-6">
+          <div className="w-full max-w-5xl">
+            <ul className="mx-auto grid grid-cols-1 gap-4
+      sm:grid-cols-2
+      md:grid-cols-3
+      lg:grid-cols-6
+      justify-items-stretch">
+
+              <GridItem
+                area="md:[grid-area:1/1/2/4] lg:[grid-area:1/1/2/5]"
+                title="TablóKereső"
+                description="I built this application for my school. It has 35 years' worth of high school graduation photos. I used Blazor and Entity Framework Core for this project."
+                gif="/tablokereso.gif"
+                href="https://krgtablok.runasp.net"
+              />
+
+              <GridItem
+                area="md:[grid-area:2/1/3/4] lg:[grid-area:2/1/3/5]"
+                title="devRoot"
+                description="I developed the devRoot project with my teammates as part of the Dusza Árpád National Programming Workshop. I was responsible for backend development and automatic deployment using ASP.NET Core and Ansible."
+                gif="/devroot.gif"
+                href="https://devroot.hu"
+              />
+
+              <GridItem
+                area="md:[grid-area:3/1/4/4] lg:[grid-area:1/5/3/7]"
+                title="MozdonyNavigator"
+                description="I developed my MozdonyNavigator application as a personal alternative to the Hungarian State Railways (MÁV) app. I plan to release it publicly in the future. Initially, I built the project using .NET MAUI with a Hybrid Blazor solution. Currently, I am in the process of rewriting it using Avalonia UI."
+                gif="/mozdonynavigator.png"
+              />
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center h-[10rem] w-full">
+        <FloatingDock
+          items={links}
+        />
+      </div>
     </div>
   );
 }
+
+const techstack = [
+  { name: ".NET Framework", image: "dotnet.svg", alt: ".NET Framework" },
+  { name: "ASP .NET", image: "aspnetcore.svg", alt: "ASP .NET Core REST API" },
+  { name: "Blazor", image: "blazor.svg", alt: "Blazor" },
+  { name: "Xamarin", image: "xamarin.svg", alt: "Xamarin" },
+  { name: ".NET MAUI", image: "maui.svg", alt: ".NET MAUI" },
+  { name: "Avalonia UI", image: "/avalonia.png", alt: "Avalonia UI" },
+  { name: "Microsoft SQL Server", image: "mssql.svg", alt: "Microsoft SQL Server" },
+  { name: "PostgreSQL", image: "postgresql.svg", alt: "PostgreSQL" },
+  { name: "Firebase", image: "firebase.svg", alt: "Google Firebase" },
+  { name: "Ansible", image: "ansible.svg", alt: "Ansible" },
+  { name: "Bash", image: "bash.svg", alt: "Bash scripting" },
+  { name: "PowerShell", image: "powershell.svg", alt: "PowerShell scripting" },
+  { name: "Docker", image: "docker.svg", alt: "Docker" },
+];
+
+
+
+interface GridItemProps {
+  area: string;
+  title: string;
+  description: React.ReactNode;
+  gif?: string;
+  href?: string | null;
+}
+const links = [
+  {
+    title: "Home",
+    icon: (
+      <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: "#",
+  },
+
+  {
+    title: "LinkedIn",
+    icon: (
+      <IconBrandLinkedin className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: "https://www.linkedin.com/in/tam%C3%A1s-solymosi-043314209",
+  },
+  {
+    title: "GitHub",
+    icon: (
+      <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: "https://github.com/EgyipTomiCreations/EgyipTomiCreations",
+  },
+  {
+    title: "Email",
+    icon: (
+      <IconMail className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: "mailto:solymositamas07@gmail.com",
+  },
+];
+
+const GridItem = ({ area, title, description, gif, href }: GridItemProps) => {
+  const renderTitle = (): React.ReactNode => {
+    if (href) {
+      return (
+        <a
+          href={href}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center hover:underline"
+        >
+          {title}
+          <FiExternalLink className="ml-2" />
+        </a>
+      );
+    }
+    return <span>{title}</span>;
+  };
+
+  return (
+    <li className={`w-full list-none ${area}`}>
+      <div className="relative flex flex-col h-full rounded-2xl border p-2 md:rounded-3xl md:p-3 bg-[rgba(40,40,40,0.80)] shadow-[2px_4px_16px_0px_rgba(248,248,248,0.06)_inset]">
+        <GlowingEffect
+          spread={40}
+          glow={true}
+          disabled={false}
+          proximity={64}
+          inactiveZone={0.01}
+        />
+        {gif && (
+          <div className="relative w-full h-40 overflow-hidden rounded-t-2xl">
+            <Image
+              alt={title}
+              src={gif}
+              layout="fill"
+              objectFit="cover"
+              className="object-cover"
+            />
+          </div>
+        )}
+        <div className="flex flex-col flex-1 justify-between gap-6 overflow-hidden p-6">
+          <div className="space-y-3">
+            <h3 className="pt-0.5 text-xl font-semibold md:text-2xl text-white">
+              {renderTitle()}
+            </h3>
+            <p className="text-sm md:text-base text-black text-neutral-400">
+              {description}
+            </p>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+};
